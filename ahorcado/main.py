81e22316a0d7e9f23f53b716e374ac4b768ad155
albernt <albertobernet@gmail.com>
2024-11-28 11:26:58 +0100

    Diccionarios de categorias con las palabras del juego

---
+++ ahorcado/main.py
@@ -0,0 +1,18 @@
+# Diccionarios de palabras organizadas en categorías
+word_categories = {
+    "Frutas": [
+        "Guayaba", "Cereza", "Ciruela", "Papaya", "Lima", "Melon", "Tamarindo", "Granada",
+        "Kiwi", "Mandarina", "Mango", "Naranja", "Pera", "Manzana", "Frambuesa", "Arandano",
+        "Higo", "Chirimoya", "Uva", "Piña", "Sandia", "Durazno", "Pomelo", "Maracuya"
+    ],
+    "Conceptos Informáticos": [
+        "Algoritmo", "Compilador", "Iteracion", "Metodo", "Variable", "Enlace", "Recursion", "Segmento",
+        "Funcion", "Objeto", "Clase", "Encapsulamiento", "Herencia", "Polimorfismo", "Framework", "Depuracion",
+        "Script", "Modulo", "Inteligencia", "Interfaz", "Repositorio", "Servidor", "Cliente", "Protocolo"
+    ],
+    "Nombres de Personas": [
+        "Elena", "Marcos", "Julia", "Carlos", "Ana", "Luis", "Sofia", "Fernando",
+        "Maria", "Javier", "Camila", "Pedro", "Isabel", "Rafael", "Lucia", "Miguel",
+        "Valeria", "Daniel", "Gabriela", "Sergio", "Claudia", "Jorge", "Natalia", "Alberto"
+    ]
+}
